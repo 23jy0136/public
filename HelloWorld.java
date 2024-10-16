0ae766a3fe43ef68f1fb2e3890235c557cbf3950
23jy0136 <23jy0136@jec.ac.jp>
2024-10-16 15:30:54 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class Helloworld{
+  public static void main(String[] args){
+    System.out.println("Helloworld");
+    System.out.println("Helloworld");
+    System.out.println("日本電子専門学校");
+}}
